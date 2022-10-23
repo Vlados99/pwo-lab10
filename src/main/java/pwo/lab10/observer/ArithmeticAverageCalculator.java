274ad112e39cf3fdf2f994d52b24025efccc8e77
Vladyslav Veshnevskyi <vladyslav.veshnevskyi@infinite.pl>
2022-10-23 15:55:57 +0200

    PWO lab10 - Zadanie 6

---
+++ src/main/java/pwo/lab10/observer/ArithmeticAverageCalculator.java
@@ -0,0 +1,22 @@
+package pwo.lab10.observer;
+
+import pwo.lab10.observer.Publisher.Subscriber;
+
+public class ArithmeticAverageCalculator implements Subscriber {
+
+    Integer sum = 0;
+    Integer length = 0;
+    Integer average = 1;
+
+    @Override
+    public void action(int x) {
+        length++;
+        sum += x;
+        
+        average = average == null ? x : sum / length;
+    }
+
+    public double getAverage() {
+        return average;
+    }
+}
--- src/main/java/pwo/lab10/observer/_run.java
+++ src/main/java/pwo/lab10/observer/_run.java
@@ -1,20 +1,23 @@
 package pwo.lab10.observer;
 
 public class _run {
-
+    
     public static void main(String[] args) {
-
+        
         NumberGetter numberGetter = new NumberGetter();
-
+        
         MaxFinder maxFinder = new MaxFinder();
         SumFinder sumFinder = new SumFinder();
-
+        ArithmeticAverageCalculator averageCalculator = new ArithmeticAverageCalculator();
+        
         numberGetter.add(maxFinder);
         numberGetter.add(sumFinder);
-
+        numberGetter.add(averageCalculator);
+        
         numberGetter.getNumbers(3);
-
+        
         System.out.println("Max: " + maxFinder.getMax());
         System.out.println("Sum: " + sumFinder.getSum());
+        System.out.println("Average: " + averageCalculator.getAverage());
     }
 }
